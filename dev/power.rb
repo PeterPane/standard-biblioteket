51d487081193268433b8fe4816ee3561fa0de514
daodac <32705304+daodac@users.noreply.github.com>
2018-05-03 13:41:45 +0200

    added description

--- dev/power.rb
+++ dev/power.rb
@@ -1,3 +1,15 @@
+# Public: Gives the number of a base to the power of an exponent
+#
+# number - The number to be used tha base
+# exponent - The number to be used as the exponent
+#
+# Examples
+#
+#   power(3,3)
+#   # => '27'
+#
+# Returns the base to the power of the exponent
+
 def power(number, exponent)
     return number**exponent
 end

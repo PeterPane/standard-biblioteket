c85ccfad91a60bb2d25d717600278fbccff07da6
daodac <32705304+daodac@users.noreply.github.com>
2018-05-03 14:09:02 +0200

    made last of

---
+++ dev/last_of.rb
@@ -0,0 +1,15 @@
+# Public: Gives the last element of a list
+#
+# list - The list in which you want to find the last element
+#
+# Examples
+#
+#   first_of("4f3,3,43w4,hsd,fsdf")
+#   # => 'fsdf'
+#
+# Returns the last element in the list
+
+def last_of(list)
+   return list[-1]
+end
+

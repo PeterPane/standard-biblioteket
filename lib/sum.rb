a0c8a42cbbd9f05a2b6e7c0b611ec4ad85b3cb39
daodac <32705304+daodac@users.noreply.github.com>
2018-05-03 14:36:50 +0200

    made sum

---
+++ lib/sum.rb
@@ -0,0 +1,22 @@
+# Public: Adds togheter the number in an array
+#
+# list - The lists of sums you want to add togheter
+ 
+#
+# Examples
+#
+#   sum([5,2,3])
+#   # => '10'
+#
+# Returns the sum of the elements in the array
+
+def sum(list)
+    ans = 0
+    i = 0
+    while i < list.length
+        ans += list[i]
+        i +=1
+    end
+    return ans
+end
+

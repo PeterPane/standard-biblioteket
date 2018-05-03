d7fe59d2c4f605cbafbe416573ef53f96650c31e
daodac <32705304+daodac@users.noreply.github.com>
2018-05-03 14:45:48 +0200

    made empty

---
+++ lib/empty.rb
@@ -0,0 +1,20 @@
+# Public: Checks if a string is empty
+#
+# list - The lists to check if it is empty
+#
+# Examples
+#
+#   empty("hej")
+#   # => 'false'
+
+#
+# Returns true or false
+
+def average(list)
+    if list == ""
+        ans = true
+    else 
+        ans = false
+    end
+    return ans
+end

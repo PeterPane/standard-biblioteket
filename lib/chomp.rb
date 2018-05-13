a33e73385403be9031d0ddf5ca46319f73235460
Philip Antonsson <32705304+daodac@users.noreply.github.com>
2018-05-13 19:36:36 +0200

    Added description

--- lib/chomp.rb
+++ lib/chomp.rb
@@ -1,3 +1,13 @@
+# Public:Removes the line break "/n"  at the end of a string
+#
+# string: The string you want to remove the line break from
+#
+# Examples
+#
+#   chomp("Hej/n")
+#   # => 'Hej'
+#
+# Returns a copy of the string without a line break
 def chomp(string)
     new_string=string
     p string[-1]
